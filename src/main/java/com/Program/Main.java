70f090b1b2fa9f2b8c8044dee78e2e01c5084c42
lyrakeen <egesevindix@hotmail.com>
2024-12-12 22:34:35 +0300

    Base GUI

--- src/main/java/com/Program/Main.java
+++ src/main/java/com/Program/Main.java
@@ -2,23 +2,74 @@ package com.Program;
 import java.util.List;
 import java.util.Set;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.Region;
 
 public class Main extends Application {
 
     VBox root = new VBox();
+    HBox toBeNext = new HBox(10);
+    HBox toCenter = new HBox();
     Scene scene = new Scene(root, 400, 200);
+    TableView table = new TableView<>();
+    MenuBar menuBar = new MenuBar();
+    Menu fileMenu = new Menu("File");
+    Menu helpMenu = new Menu("Help");
+    MenuItem importItem = new MenuItem("Import Files");
+    MenuItem teacherItem = new MenuItem("Manage Teachers");
+    MenuItem studentItem = new MenuItem("Manage Students");
+    MenuItem courseItem = new MenuItem("Manage Courses");
+    MenuItem saveItem = new MenuItem("Save");
+    MenuItem quitItem = new MenuItem("Quit");
+    MenuItem aboutItem = new MenuItem("About");
+    MenuItem manualItem = new MenuItem("Manual");
+    Region spacer1 = new Region();
+    Region spacer2 = new Region();
+    Region spacer3 = new Region();
+    Label sketchuler = new Label("Sketchuler");
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage firstStage) {
+        menuBar.getMenus().addAll(fileMenu, helpMenu);
+        fileMenu.getItems().addAll(importItem,teacherItem,studentItem,courseItem,saveItem,quitItem);
+        helpMenu.getItems().addAll(aboutItem,manualItem);
+        Button mainProceed = new Button("Proceed");
+        ComboBox<String> selection = new ComboBox<>();
+        selection.getItems().addAll("Courses", "Students", "Teachers", "Classes");
+        selection.setOnAction(e -> {
+            String selected = selection.getValue(); 
+            System.out.println("Seçilen meyve: " + selected);
+        });
+        toCenter.getChildren().add(sketchuler);
+        toBeNext.getChildren().addAll(selection, mainProceed);
+        root.getChildren().addAll(menuBar,spacer1,toCenter,spacer3,toBeNext,spacer2,table);
+        toBeNext.setAlignment(Pos.CENTER);
+        toCenter.setAlignment(Pos.CENTER);
+        HBox.setHgrow(spacer1, Priority.ALWAYS);
+        VBox.setVgrow(spacer1, Priority.ALWAYS);
+        HBox.setHgrow(spacer2, Priority.ALWAYS);
+        VBox.setVgrow(spacer2, Priority.ALWAYS);
+        HBox.setHgrow(spacer3, Priority.ALWAYS);
+        VBox.setVgrow(spacer3, Priority.ALWAYS);
+        HBox.setHgrow(table, Priority.ALWAYS);
+        VBox.setVgrow(table, Priority.ALWAYS);
 
-        primaryStage.setTitle("Sketchuler");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        firstStage.setTitle("Sketchuler");
+        firstStage.setScene(scene);
+        firstStage.show();
     }
 
     public static void main(String[] args) {
